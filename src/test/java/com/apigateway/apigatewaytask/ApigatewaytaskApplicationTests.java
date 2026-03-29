c4d20f281eb29efb19b27c2450971ef0b27ff860
Prakhar Tripathi <talk2prakhartripathi@gmail.com>
2026-03-29 18:56:06 +0530

    initial commit

---
+++ src/test/java/com/apigateway/apigatewaytask/ApigatewaytaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigateway.apigatewaytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewaytaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
